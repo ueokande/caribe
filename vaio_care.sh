f8567e159a7ebbeb2ba69e5d23978cafd3431e11
Shin'ya Ueoka <ibenza@i-beam.org>
2015-09-29 16:18:42 +0900

    Add option to resume on lid open

--- vaio_care.sh
+++ vaio_care.sh
@@ -49,16 +49,31 @@ kbd_backlight() {
   fi
 }
 
+lid_resume() {
+  file='/sys/devices/platform/sony-laptop/lid_resume_S5'
+  current_value=$(<$file)
+  dialog --radiolist 'Enable/disable resume on lid open' \
+                     0 60 0 \
+                     '0' 'Disable' `on_off 0 "$current_value"`\
+                     '1' 'Enable' `on_off 1 "$current_value"`\
+         2>$tmp
+  if [ $? == 0 ]; then
+    echo `cat $tmp` >$file || exit 1
+  fi
+}
+
 mainmenu() {
   battery_care='Battery Care'
   fan_controll='Fan Controll'
   kbd_backlight='Keyboard Backlight'
+  lid_resume='Resume on lid open'
   while true; do
     dialog --title 'VAIO Care' \
            --menu 'Select an item to configure' 0 60 0 \
                   "$battery_care" 'Set the maximum of the battery charge' \
                   "$fan_controll" 'Set the fan controlls' \
                   "$kbd_backlight" 'Enable/disable the keyboard backlight' \
+                  "$lid_resume" 'Enable/disable resume on lid open' \
            2>$tmp
     if [ "$?" != "0" ]; then return; fi
 
@@ -67,6 +82,7 @@ mainmenu() {
       "$battery_care") battery_care;;
       "$fan_controll") fan_controll;;
       "$kbd_backlight") kbd_backlight;;
+      "$lid_resume") lid_resume;;
     esac
   done
 }
