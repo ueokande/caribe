82d442a9fd3ddc89f7af58534f1c8cffe5187a3c
Shin'ya Ueoka <ueokande@i-beam.org>
2016-01-26 23:09:41 +0900

    Pass shellcheck as lint

--- vaio_care.sh
+++ vaio_care.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 on_off() {
   if [ $# -lt 2 ]; then
     echo 'off'
-  elif [ $1 = $2 ]; then
+  elif [ "$1" -eq "$2" ]; then
     echo 'on'
   else
     echo 'off'
@@ -10,55 +12,67 @@ on_off() {
 
 battery_care() {
   file='/sys/devices/platform/sony-laptop/battery_care_limiter'
-  current_value=$(<$file)
-  dialog --radiolist 'Set the fan contolls' \
-                     0 60 0 \
-                     '50' 'Battery care (50%)' `on_off 50 "$current_value"`\
-                     '80' 'Battery care (80%)' `on_off 80 "$current_value"`\
-                     '0' 'Disable battery care' `on_off 0 "$current_value"`\
-         2>$tmp
-  if [ $? == 0 ]; then
-    echo `cat $tmp` >$file || exit 1
+  current_value=$(cat $file)
+  choice=$(
+    dialog --radiolist \
+           'Set the fan contolls' \
+           0 60 0 \
+           '50' 'Battery care (50%)' "$(on_off 50 "$current_value")"\
+           '80' 'Battery care (80%)' "$(on_off 80 "$current_value")"\
+           '0' 'Disable battery care' "$(on_off 0 "$current_value")"\
+           2>&1 >/dev/tty
+  )
+  if [ $? -eq 0 ]; then
+    echo "$choice" >$file || exit 1
   fi
 }
 
 fan_controll() {
   file='/sys/devices/platform/sony-laptop/thermal_control'
-  current_value=$(<$file)
-  dialog --radiolist 'Set the fan contolls' \
-                     0 60 0 \
-                     'balanced' 'Balanced performance mode' `on_off balanced "$current_value"`\
-                     'silent' 'Silent fan mode' `on_off silent "$current_value"`\
-                     'performance' 'High performance mode' `on_off performance "$current_value"`\
-         2>$tmp
-  if [ $? == 0 ]; then
-    echo `cat $tmp` >$file || exit 1
+  current_value=$(cat $file)
+  choice=$(
+    dialog --radiolist \
+           'Set the fan contolls' \
+           0 60 0 \
+           'balanced' 'Balanced performance mode' "$(on_off balanced "$current_value")" \
+           'silent' 'Silent fan mode' "$(on_off silent "$current_value")" \
+           'performance' 'High performance mode' "$(on_off performance "$current_value")" \
+           2>&1 >/dev/tty
+  )
+  if [ $? -eq 0 ]; then
+    echo "$choice" >$file || exit 1
   fi
 }
 
 kbd_backlight() {
   file='/sys/devices/platform/sony-laptop/kbd_backlight'
-  current_value=$(<$file)
-  dialog --radiolist 'Enable/disable the keyboard backlight' \
-                     0 60 0 \
-                     '0' 'Disable' `on_off 0 "$current_value"`\
-                     '1' 'Enable' `on_off 1 "$current_value"`\
-         2>$tmp
-  if [ $? == 0 ]; then
-    echo `cat $tmp` >$file || exit 1
+  current_value=$(cat<$file)
+  choice=$(
+    dialog --radiolist \
+           'Enable/disable the keyboard backlight' \
+           0 60 0 \
+           '0' 'Disable' "$(on_off 0 "$current_value")" \
+           '1' 'Enable' "$(on_off 1 "$current_value")" \
+           2>&1 >/dev/tty
+  )
+  if [ $? -eq 0 ]; then
+    echo "$choice" >$file || exit 1
   fi
 }
 
 lid_resume() {
   file='/sys/devices/platform/sony-laptop/lid_resume_S5'
-  current_value=$(<$file)
-  dialog --radiolist 'Enable/disable resume on lid open' \
-                     0 60 0 \
-                     '0' 'Disable' `on_off 0 "$current_value"`\
-                     '1' 'Enable' `on_off 1 "$current_value"`\
-         2>$tmp
-  if [ $? == 0 ]; then
-    echo `cat $tmp` >$file || exit 1
+  current_value=$(cat $file)
+  choice=$(
+    dialog --radiolist \
+           'Enable/disable resume on lid open' \
+           0 60 0 \
+           '0' 'Disable' "$(on_off 0 "$current_value")"\
+           '1' 'Enable' "$(on_off 1 "$current_value")"\
+           2>&1 >/dev/tty
+  )
+  if [ $? -eq 0 ]; then
+    echo "$choice" >$file || exit 1
   fi
 }
 
@@ -68,16 +82,16 @@ mainmenu() {
   kbd_backlight='Keyboard Backlight'
   lid_resume='Resume on lid open'
   while true; do
-    dialog --title 'VAIO Care' \
-           --menu 'Select an item to configure' 0 60 0 \
-                  "$battery_care" 'Set the maximum of the battery charge' \
-                  "$fan_controll" 'Set the fan controlls' \
-                  "$kbd_backlight" 'Enable/disable the keyboard backlight' \
-                  "$lid_resume" 'Enable/disable resume on lid open' \
-           2>$tmp
+    choice=$(
+      dialog --title 'VAIO Care' \
+             --menu 'Select an item to configure' 0 60 0 \
+                    "$battery_care" 'Set the maximum of the battery charge' \
+                    "$fan_controll" 'Set the fan controlls' \
+                    "$kbd_backlight" 'Enable/disable the keyboard backlight' \
+                    "$lid_resume" 'Enable/disable resume on lid open' \
+             2>&1 >/dev/tty
+    )
     if [ "$?" != "0" ]; then return; fi
-
-    choice=$(< $tmp)
     case $choice in
       "$battery_care") battery_care;;
       "$fan_controll") fan_controll;;
@@ -86,9 +100,6 @@ mainmenu() {
     esac
   done
 }
-
-tmp=`mktemp`
-trap "rm $tmp" EXIT
 
 mainmenu
 clear
